3eec067849b02c51ccd123c898217c6b6adb024b
Thomas Grimonet <tgrimonet@arista.com>
2022-10-11 15:01:04 +0000

    make: Update check lab status script to use docker

--- check-lab-running.sh
+++ check-lab-running.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-CLAB=$(which containerlab)
+DOCKER=$(which docker)
 LAB_PREFIX=${1:-clab-fabric-avd}
 
-echo "🪐 Containerlab path: ${CLAB}"
+echo "🪐 Docker path: ${DOCKER}"
 echo "❓ Lab prefix is: ${LAB_PREFIX}"
 
-NODES_RUNNING=$(sudo $CLAB inspect --all | grep $LAB_PREFIX | wc -l)
+NODES_RUNNING=$($DOCKER ps | grep clab-fabric-avd | wc -l)
 echo "🔎 Number of nodes running: ${NODES_RUNNING}"
 if (( ${NODES_RUNNING:0} == 0 )); then
     echo "❌ Lab is not running !"
